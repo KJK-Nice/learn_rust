25fc45ee9da014935740fe565f89a843511b5b85
khajornkiat <khajornkiat.us@ols.co.th>
2023-09-18 09:31:15 +0700

    add if let and matching example

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -3,10 +3,61 @@ enum IpAddr {
     V6(String),
 }
 
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", &self);
+    }
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
 fn main() {
-    let home = IpAddr:V4(String::from("127.0.0.1"));
+    let home = IpAddr::V4(String::from("127.0.0.1"));
     let loopback = IpAddr::V6(String::from("::1"));
 
     println!("Hello, world!");
+
+    let m = Message::Write(String::from("Hi there"));
+    m.call();
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let config_max = Some(3u8);
+    let max: 3u8 = 5;
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
 }
+
 
